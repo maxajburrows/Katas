21f9f0ae5cc16bbb3a33548f866c9cc34ac85eb1
maxaj <maxaj@Xubuntu.myguest.virtualbox.org>
2023-08-18 22:15:31 +0200

    Complete Digital Root kata

---
+++ src/main/java/DigitalRoot.java
@@ -0,0 +1,18 @@
+public class DigitalRoot {
+
+    public int reduceSums(int num) {
+        int digitSum = sumDigitsWithRec(0, String.valueOf(num));
+        if (digitSum >= 10) {
+            return reduceSums(digitSum);
+        }
+        return digitSum;
+    }
+
+    public int sumDigitsWithRec(int sum, String remainNumber) {
+        sum += Integer.valueOf(remainNumber.substring(0, 1));
+        if (remainNumber.length() > 1) {
+            return sumDigitsWithRec(sum, remainNumber.substring(1));
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/DigitalRootTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DigitalRootTest {
+
+    @Test
+    public void returns7For16() {
+        DigitalRoot digitalRoot = new DigitalRoot();
+        assertEquals(7, digitalRoot.reduceSums(16));
+    }
+
+    @Test
+    public void returns2For493193() {
+        DigitalRoot digitalRoot = new DigitalRoot();
+        assertEquals(2, digitalRoot.reduceSums(493193));
+    }
+}
