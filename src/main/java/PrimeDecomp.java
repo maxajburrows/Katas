4c1f0221c91ed94ed593509464b01fcfa1af8a31
maxaj <maxaj@Xubuntu.myguest.virtualbox.org>
2023-08-29 23:48:41 +0200

    Start prime decomposition kata

---
+++ src/main/java/PrimeDecomp.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class PrimeDecomp {
+
+    public String decompose(int n) {
+        String decomposedString = "";
+    }
+
+    public ArrayList<String> findPrimes(int n) {
+
+    }
+}
