683926247ffbd13a91a11b03a5974e5e1781db5a
Max Burrows <maxajburrows@gmail.com>
2023-11-21 18:35:21 +0100

    Set up complete structure for completing kata

--- src/main/java/KnightsShortestPath.java
+++ src/main/java/KnightsShortestPath.java
@@ -1,12 +1,89 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class KnightsShortestPath {
     public static int knight(String start, String finish) {
-        return 0;
+        String[] startSplit = start.split("");
+        if (Integer.parseInt(startSplit[1]) < 1 || Integer.parseInt(startSplit[1]) > 8) {
+            throw new IllegalArgumentException("Given square is not within the board!");
+        }
+        // Add same check for letters and end square
+        String[] endSplit = start.split("");
+        return moveOptions(new ArrayList<>(), startSplit, endSplit, 0);
     }
-    public moveOptions(List<String> previousSquares, String currentSquare) {
+    public static int moveOptions(List<String> previousSquares, String[] currentSquare, String[] endSquare, int numMoves) {
         // 8 move options
         // Break out if you hit a square that has already been landed on
         // Maybe add the current best score
+        if (Arrays.equals(currentSquare, endSquare)) {
+            return numMoves;
+        }
+
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        int count4 = 0;
+        int count5 = 0;
+        int count6 = 0;
+        int count7 = 0;
+        int count8 = 0;
+        // Check if current square is the same as any previous square. If so break
+
+        Integer squareNum = Integer.parseInt(currentSquare[1]);
+        if (!currentSquare[0].equals("h") && !currentSquare[1].equals("8")) {
+            count1 = move1(previousSquares, currentSquare, endSquare, numMoves+1);
+            count2 = move2(previousSquares, currentSquare, endSquare, numMoves+1);
+        }
+
+        // option 1
+
+
+
+        // Find maximum of all counts
+
+        return 0;
+    }
+
+    public static int move(List<String> previousSquares, String[] currentSquare, String[] endSquare, int numMoves, int numInc, int letInc) {
+        String[] move = new String[] { changeLetter(currentSquare[0], letInc),
+                String.valueOf (Integer.parseInt(currentSquare[1])+numInc) };
+        List<String> previousSquares1 = new ArrayList<>();
+        previousSquares.stream().map(previousSquares1::add);
+        return moveOptions(previousSquares1, move, endSquare, numMoves + 1);
+    }
+
+    private static String changeLetter(String currentValue, int increment) {
+        return String.valueOf((currentValue.toCharArray()[0]) + increment);
+    }
+
+    public static int move1(List<String> previousSquares, String[] currentSquare, String[] endSquare, int numMoves) {
+        return move(previousSquares, currentSquare, endSquare, numMoves, 2, 1);
+    }
+
+    public static int move2(List<String> previousSquares, String[] currentSquare, String[] endSquare, int numMoves) {
+        return move(previousSquares, currentSquare, endSquare, numMoves, 1, 2);
+    }
+
+    public static int move3(List<String> previousSquares, String[] currentSquare, String[] endSquare, int numMoves) {
+        return move(previousSquares, currentSquare, endSquare, numMoves, -2, 1);
+    }
+
+    public static int move4(List<String> previousSquares, String[] currentSquare, String[] endSquare, int numMoves) {
+        return move(previousSquares, currentSquare, endSquare, numMoves, -1, 2);
+    }
+
+    public static int move5(List<String> previousSquares, String[] currentSquare, String[] endSquare, int numMoves) {
+        return move(previousSquares, currentSquare, endSquare, numMoves, 2, -1);
+    }
+    public static int move6(List<String> previousSquares, String[] currentSquare, String[] endSquare, int numMoves) {
+        return move(previousSquares, currentSquare, endSquare, numMoves, 1, -2);
+    }
+    public static int move7(List<String> previousSquares, String[] currentSquare, String[] endSquare, int numMoves) {
+        return move(previousSquares, currentSquare, endSquare, numMoves, -1, -2);
+    }
+
+    public static int move8(List<String> previousSquares, String[] currentSquare, String[] endSquare, int numMoves) {
+        return move(previousSquares, currentSquare, endSquare, numMoves, -2, -1);
     }
 }
