335ad5a293e5c14330a59c8b442ebbeb78e9e750
Max Burrows <maxajburrows@gmail.com>
2023-11-23 15:45:38 +0100

    Restart solution with variation of breadth first traversal

---
+++ src/main/java/KnightsShortestPath/KnightsShortestPath.java
@@ -0,0 +1,10 @@
+package KnightsShortestPath;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class KnightsShortestPath {
+
+}
