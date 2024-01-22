ab6e3c34fb0a0bd6c0b1c8fddc825b910cbe9e29
Max Burrows <maxajburrows@gmail.com>
2024-01-22 21:01:55 +0100

    Deal with empty input for merged linked list kata

---
+++ src/main/java/MergeKLinkedLists/MergeKLinkedLists.java
@@ -0,0 +1,17 @@
+package MergeKLinkedLists;
+
+import java.util.Arrays;
+
+public class MergeKLinkedLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0 || lists[0] == null) {
+            return null;
+        }
+        int lowValue = -1000;
+        for (int i = 0; i < lists.length; i++) {
+            if (lists)
+        }
+    }
+
+}
