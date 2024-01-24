ee154545d7b53ceb3182cb42b98d3d1098fbdaa9
Max Burrows <maxajburrows@gmail.com>
2024-01-24 17:20:55 +0100

    Find working solution to kata

--- src/main/java/BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSum.java
+++ src/main/java/BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSum.java
@@ -1,9 +1,26 @@
 package BinaryTreeMaximumPathSum;
 
+import java.util.Arrays;
+
 public class BinaryTreeMaximumPathSum {
 
-    public int findMaxPath() {
-        return 0;
+    public int findMaxPath(TreeNode root) {
+
+        // Traverse left hand side
+        int leftTreeMax = 0;
+        if (root.left != null) {
+            leftTreeMax = findMaxPath(root.left);
+        }
+
+        // Traverse Right hand side
+        int rightTreeMax = 0;
+        if (root.left != null) {
+            rightTreeMax = findMaxPath(root.right);
+        }
+
+        // Decide whether to add together
+        int[] permutations = new int[] {leftTreeMax, rightTreeMax, leftTreeMax+ root.val, rightTreeMax+ root.val, leftTreeMax+ root.val+rightTreeMax};
+        return Arrays.stream(permutations).max().orElseThrow();
     }
 
 }
