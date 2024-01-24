9f581b695f1db0327373df3e76f230104c4daadc
Max Burrows <maxajburrows@gmail.com>
2024-01-24 17:44:22 +0100

    Simplify solution and pass more test cases

--- src/main/java/BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSum.java
+++ src/main/java/BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSum.java
@@ -4,23 +4,22 @@ import java.util.Arrays;
 
 public class BinaryTreeMaximumPathSum {
 
-    public int findMaxPath(TreeNode root) {
 
-        // Traverse left hand side
-        int leftTreeMax = 0;
-        if (root.left != null) {
-            leftTreeMax = findMaxPath(root.left);
+    public int maxPathSum(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return root.val;
         }
 
-        // Traverse Right hand side
+        int leftTreeMax = 0;
         int rightTreeMax = 0;
         if (root.left != null) {
-            rightTreeMax = findMaxPath(root.right);
+            leftTreeMax = maxPathSum(root.left);
+        }
+        if (root.right != null) {
+            rightTreeMax = maxPathSum(root.right);
         }
 
-        // Decide whether to add together
         int[] permutations = new int[] {leftTreeMax, rightTreeMax, leftTreeMax+ root.val, rightTreeMax+ root.val, leftTreeMax+ root.val+rightTreeMax};
         return Arrays.stream(permutations).max().orElseThrow();
     }
-
 }
