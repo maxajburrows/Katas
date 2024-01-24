2ea80cbbf8207b78f6c98d16623a6a1666b62c47
Max Burrows <maxajburrows@gmail.com>
2024-01-24 18:03:00 +0100

    Realise solution doesn't work - sad times

--- src/main/java/BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSum.java
+++ src/main/java/BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSum.java
@@ -3,23 +3,23 @@ package BinaryTreeMaximumPathSum;
 import java.util.Arrays;
 
 public class BinaryTreeMaximumPathSum {
-
-
     public int maxPathSum(TreeNode root) {
         if (root.left == null && root.right == null) {
             return root.val;
         }
-
-        int leftTreeMax = 0;
-        int rightTreeMax = 0;
+        if (root.left != null && root.right != null) {
+            int leftTreeMax = maxPathSum(root.left);
+            int rightTreeMax = maxPathSum(root.right);
+            int[] permutations = new int[] {leftTreeMax, rightTreeMax, leftTreeMax + root.val, rightTreeMax + root.val, leftTreeMax+ root.val + rightTreeMax, root.val};
+            return Arrays.stream(permutations).max().orElseThrow();
+        }
         if (root.left != null) {
-            leftTreeMax = maxPathSum(root.left);
+            int leftTreeMax = maxPathSum(root.left);
+            int[] permutations = new int[] {leftTreeMax, leftTreeMax + root.val, root.val};
+            return Arrays.stream(permutations).max().orElseThrow();
         }
-        if (root.right != null) {
-            rightTreeMax = maxPathSum(root.right);
-        }
-
-        int[] permutations = new int[] {leftTreeMax, rightTreeMax, leftTreeMax+ root.val, rightTreeMax+ root.val, leftTreeMax+ root.val+rightTreeMax};
+        int rightTreeMax = maxPathSum(root.right);
+        int[] permutations = new int[] {rightTreeMax, rightTreeMax + root.val, root.val};
         return Arrays.stream(permutations).max().orElseThrow();
     }
 }
