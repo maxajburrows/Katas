265dde0643f218c7c02eda095d6f83aaa9426f67
Max Burrows <maxajburrows@gmail.com>
2024-01-24 18:22:36 +0100

    Pass all test cases

--- src/main/java/BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSum.java
+++ src/main/java/BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSum.java
@@ -4,22 +4,32 @@ import java.util.Arrays;
 
 public class BinaryTreeMaximumPathSum {
     public int maxPathSum(TreeNode root) {
+        return Arrays.stream(maxPathSumHelper(root)).max().orElseThrow();
+    }
+
+    // Return unbroken paths at 0 index and broken paths at 1 index
+    private int[] maxPathSumHelper(TreeNode root) {
         if (root.left == null && root.right == null) {
-            return root.val;
+            return new int[]{root.val, root.val};
         }
         if (root.left != null && root.right != null) {
-            int leftTreeMax = maxPathSum(root.left);
-            int rightTreeMax = maxPathSum(root.right);
-            int[] permutations = new int[] {leftTreeMax, rightTreeMax, leftTreeMax + root.val, rightTreeMax + root.val, leftTreeMax+ root.val + rightTreeMax, root.val};
-            return Arrays.stream(permutations).max().orElseThrow();
+            int[] leftTreeMax = maxPathSumHelper(root.left);
+            int[] rightTreeMax = maxPathSumHelper(root.right);
+            int[] maxUnbrokenPathPermutations = new int[] {leftTreeMax[0] + root.val, rightTreeMax[0] + root.val, root.val};
+            int[] maxBrokenPathPermutations = new int[] {leftTreeMax[0], rightTreeMax[0], leftTreeMax[0] + root.val, rightTreeMax[0] + root.val, leftTreeMax[0] + root.val + rightTreeMax[0], root.val, leftTreeMax[1], rightTreeMax[1]};
+            return new int[] {Arrays.stream(maxUnbrokenPathPermutations).max().orElseThrow(), Arrays.stream(maxBrokenPathPermutations).max().orElseThrow()};
         }
         if (root.left != null) {
-            int leftTreeMax = maxPathSum(root.left);
-            int[] permutations = new int[] {leftTreeMax, leftTreeMax + root.val, root.val};
-            return Arrays.stream(permutations).max().orElseThrow();
+            int[] leftTreeMax = maxPathSumHelper(root.left);
+            int[] maxUnbrokenPathPermutations = new int[] {leftTreeMax[0] + root.val, root.val};
+            int[] maxBrokenPathPermutations = new int[] {leftTreeMax[0], leftTreeMax[0] + root.val, root.val, leftTreeMax[1]};
+            return new int[] {Arrays.stream(maxUnbrokenPathPermutations).max().orElseThrow(), Arrays.stream(maxBrokenPathPermutations).max().orElseThrow()};
         }
-        int rightTreeMax = maxPathSum(root.right);
-        int[] permutations = new int[] {rightTreeMax, rightTreeMax + root.val, root.val};
-        return Arrays.stream(permutations).max().orElseThrow();
+        int[] rightTreeMax = maxPathSumHelper(root.right);
+        int[] maxUnbrokenPathPermutations = new int[] {rightTreeMax[0] + root.val, root.val};
+        int[] maxBrokenPathPermutations = new int[] {rightTreeMax[0], rightTreeMax[0] + root.val, root.val, rightTreeMax[1]};
+        return new int[] {Arrays.stream(maxUnbrokenPathPermutations).max().orElseThrow(), Arrays.stream(maxBrokenPathPermutations).max().orElseThrow()};
     }
+
 }
+
