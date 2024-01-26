949c47a14acd8be130e402aa28df3a271ac1dc73
Max Burrows <maxajburrows@gmail.com>
2024-01-26 20:22:54 +0100

    Set up n queens kata

---
+++ src/main/java/NQueens.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class NQueens {
+
+
+    public List<List<String>> solveNQueens(int n) {
+        // Build Board
+
+        // Add Queen to Board
+
+        // Work out blocked square
+
+        // Translate into string
+    }
+}
