3a583c33debe21bca8683787b233b64f83fe3fb7
Max Burrows <maxajburrows@gmail.com>
2024-01-29 12:18:49 +0100

    Set up implement queue kata

---
+++ src/main/java/ImplementQueueUsingStacks.java
@@ -0,0 +1,21 @@
+public class ImplementQueueUsingStacks {
+
+    public ImplementQueueUsingStacks() {
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int peek() {
+
+    }
+
+    public boolean empty() {
+
+    }
+}
