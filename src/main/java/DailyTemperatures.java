7d005bea3ba3dab497037b20637402a8b517a583
Max Burrows <maxajburrows@gmail.com>
2024-01-31 14:14:26 +0100

    Start solving daily temperatures kata

--- src/main/java/DailyTemperatures.java
+++ src/main/java/DailyTemperatures.java
@@ -1,5 +1,11 @@
 public class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
+        int arrayLength = temperatures.length;
+        int[] answersArray = new int[arrayLength];
+
+        for (int i=0; i < arrayLength; i++) {
+            int comparisonTemp = temperatures[i];
+        }
 
     }
 }
