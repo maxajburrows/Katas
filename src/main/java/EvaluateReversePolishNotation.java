ddc24ea079e65e1e2e06471b81d6a9b8650464fa
Max Burrows <maxajburrows@gmail.com>
2024-01-31 01:14:48 +0100

    Redo kata with stack solution

--- src/main/java/EvaluateReversePolishNotation.java
+++ src/main/java/EvaluateReversePolishNotation.java
@@ -1,5 +1,28 @@
 import java.util.Objects;
+import java.util.Stack;
 
 public class EvaluateReversePolishNotation {
+    Stack<Integer> stack = new Stack<>();
+    public int evalRPN(String[] tokens) {
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    stack.push(- stack.pop() + stack.pop());
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    stack.push((1/stack.pop()) * stack.pop());
+                    break;
+                default:
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
 
 }
