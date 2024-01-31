ad4cd38c60da834333d0a4606a05a4b4b8d9f127
Max Burrows <maxajburrows@gmail.com>
2024-01-31 14:57:30 +0100

    Write algorithm

--- src/main/java/DailyTemperatures.java
+++ src/main/java/DailyTemperatures.java
@@ -4,8 +4,16 @@ public class DailyTemperatures {
         int[] answersArray = new int[arrayLength];
 
         for (int i=0; i < arrayLength; i++) {
+            int counter = 0;
             int comparisonTemp = temperatures[i];
+            for (int j=1; i+j < arrayLength; j++) {
+                counter++;
+                if (temperatures[i+j] == comparisonTemp) {
+                    break;
+                }
+            }
+            answersArray[i] = counter;
         }
-
+        return answersArray;
     }
 }
