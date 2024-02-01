4e2f61569e15713ade07d566904ee2d70c8eb5b9
Max Burrows <maxajburrows@gmail.com>
2024-02-01 22:33:14 +0100

    Start solving median of two sorted arrays kata

--- src/main/java/MedianTwoSortedArrays.java
+++ src/main/java/MedianTwoSortedArrays.java
@@ -1,9 +1,28 @@
 public class MedianTwoSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Merge arrays
-        // Find median
-        // Gives O(m+n) - not good enough
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        int totalLength = length1 + length2;
+        double median1 = calculateMedian(nums1);
+        double median2 = calculateMedian(nums2);
+
+        // Find distance of median of first array from median of second array
+        double indexDistance = findIndexOffset(nums2, median1);
 
         return 0;
+    }
+
+    private double findIndexOffset(int[] nums, double median) {
+
+    }
+
+    public double calculateMedian(int[] nums) {
+        double median;
+        if (nums.length % 2 == 0) {
+            median = (double) (nums[nums.length / 2 - 1] + nums[nums.length / 2]) /2;
+        } else {
+            median = nums[nums.length/2];
+        }
+        return median;
     }
 }
