89fadba8da03fd311ccad299bcac9a838a1426e5
Max Burrows <maxajburrows@gmail.com>
2024-02-01 14:31:18 +0100

    Final divide array submission

--- src/main/java/DivideArray.java
+++ src/main/java/DivideArray.java
@@ -3,12 +3,13 @@ import java.util.Arrays;
 public class DivideArray {
     public int[][] divideArray(int[] nums, int k) {
         Arrays.sort(nums);
-        int[][] answerArray = new int[nums.length/3][];
-
         for (int i=0; i < nums.length/3; i++) {
             if (nums[i*3]+k < nums[i*3+2]) {
                 return new int[0][0];
             }
+        }
+        int[][] answerArray = new int[nums.length/3][];
+        for (int i=0; i < nums.length/3; i++) {
             int[] indexArray = new int[3];
             System.arraycopy(nums, i * 3, indexArray, 0, 3);
             answerArray[i] = indexArray;
