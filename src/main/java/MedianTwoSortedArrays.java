b516bb37c28c71bbe7a50efbea93ec2e56484e4d
Max Burrows <maxajburrows@gmail.com>
2024-02-01 21:44:11 +0100

    Set up median or two sorted arrays kata

---
+++ src/main/java/MedianTwoSortedArrays.java
@@ -0,0 +1,9 @@
+public class MedianTwoSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Merge arrays
+        // Find median
+        // Gives O(m+n) - not good enough
+
+        return 0;
+    }
+}
