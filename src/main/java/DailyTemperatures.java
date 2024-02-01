adeaa6370f3e6f1710fe26f338fa9a7fcfc2484c
Max Burrows <maxajburrows@gmail.com>
2024-02-01 20:20:30 +0100

    Try monotonic stack solution

--- src/main/java/DailyTemperatures.java
+++ src/main/java/DailyTemperatures.java
@@ -1,44 +1,9 @@
+import java.util.Stack;
+
 public class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
-        int arrayLength = temperatures.length;
-        int[] answersArray = new int[arrayLength];
 
-        for (int i=0; i < arrayLength; i++) {
-            int counter = 0;
-            int comparisonTemp = temperatures[i];
-            boolean nothingLower = true;
-            boolean foundHigher = false;
-            int lowerIndex = 0;
-            for (int j=1; i+j < arrayLength; j++) {
-                counter++;
-                if (temperatures[i+j] < comparisonTemp && nothingLower) {
-                    lowerIndex = i+j;
-                    nothingLower = false;
-                }
-                if (temperatures[i+j] > comparisonTemp) {
-                    foundHigher = true;
-                    break;
-                }
-            }
-            if (!foundHigher) {
-                if (nothingLower) {
-                    return answersArray;
-                }
-                i = lowerIndex-1;
-            } else {
-                answersArray[i] = counter;
-                if (nothingLower && i < arrayLength-1) {
-                    int countdown = counter;
-                    for (int j=i+1; j < i+counter; j++) {
-                        answersArray[j] = --countdown;
-                    }
-                    if (i + counter == arrayLength-1) {
-                        return answersArray;
-                    }
-                    i += (counter-1);
-                }
-            }
-        }
-        return answersArray;
+
+        return null;
     }
 }
