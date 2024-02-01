de19b2ea184d19f607750ab05518ce0b91c587c4
Max Burrows <maxajburrows@gmail.com>
2024-02-01 21:14:55 +0100

    Pass first test cases with monotonic stack solution

--- src/main/java/DailyTemperatures.java
+++ src/main/java/DailyTemperatures.java
@@ -1,9 +1,20 @@
+import java.util.ArrayDeque;
 import java.util.Stack;
 
 public class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
-
-
-        return null;
+        ArrayDeque<Integer> indexStack = new ArrayDeque<>();
+        int length = temperatures.length;
+        int[] answersArray = new int[length];
+        for (int i = length-1; i >=0; i--) {
+            while (!indexStack.isEmpty() && temperatures[i] > temperatures[indexStack.peek()]) {
+                indexStack.pop();
+            }
+            if (!indexStack.isEmpty()) {
+                answersArray[i] = indexStack.peek()-i;
+            }
+            indexStack.push(i);
+        }
+        return answersArray;
     }
 }
