4c4217b0ea2ffafcd4e78b48301241884ec35b4a
Max Burrows <maxajburrows@gmail.com>
2024-02-01 13:51:32 +0100

    Finish first algorithm attempt

--- src/main/java/DivideArray.java
+++ src/main/java/DivideArray.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class DivideArray {
     public int[][] divideArray(int[] nums, int k) {
-        return null;
+        Arrays.sort(nums);
+        int[][] answerArray = new int[nums.length/k][3];
+
+        for (int i=0; i < nums.length/k; i++) {
+            int[] indexArray = new int[3];
+            // Can replace with System.arraycopy later
+            for (int j=0; j < 3; j++) {
+                indexArray[j] = nums[i*3+j];
+            }
+            if (indexArray[0] + k < indexArray[2]) {
+                return new int[0][0];
+            }
+            answerArray[i] = indexArray;
+        }
+        return answerArray;
     }
 }
