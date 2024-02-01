a3c245219768cf781aab1bfa12120d39fb93b679
Max Burrows <maxajburrows@gmail.com>
2024-02-01 19:49:13 +0100

    Slightly improve runtime of daily temperatures solution

--- src/main/java/DailyTemperatures.java
+++ src/main/java/DailyTemperatures.java
@@ -7,7 +7,6 @@ public class DailyTemperatures {
             int counter = 0;
             int comparisonTemp = temperatures[i];
             boolean nothingLower = true;
-//            boolean lowerLoop = false;
             boolean foundHigher = false;
             int lowerIndex = 0;
             for (int j=1; i+j < arrayLength; j++) {
@@ -20,23 +19,14 @@ public class DailyTemperatures {
                     foundHigher = true;
                     break;
                 }
-//                if (i+j == arrayLength-1) {
-//                    if (nothingLower && i < arrayLength-1) {
-//                        return answersArray;
-//                    }
-//                    answersArray[i] = 0;
-//                    lowerLoop = true;
-//                }
             }
             if (!foundHigher) {
                 if (nothingLower) {
                     return answersArray;
                 }
                 i = lowerIndex-1;
-//                answersArray[i] = 0;
             } else {
                 answersArray[i] = counter;
-//            answersArray[i] = counter;
                 if (nothingLower && i < arrayLength-1) {
                     int countdown = counter;
                     for (int j=i+1; j < i+counter; j++) {
@@ -45,12 +35,9 @@ public class DailyTemperatures {
                     if (i + counter == arrayLength-1) {
                         return answersArray;
                     }
-
+                    i += (counter-1);
                 }
-
-                //i += (counter-1);
             }
-
         }
         return answersArray;
     }
