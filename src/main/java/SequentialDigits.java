a9b27b8ae5616469800533d60735e2bc2a3edbf3
Max Burrows <maxajburrows@gmail.com>
2024-02-02 13:23:23 +0100

    Pass all test cases with fast solution

--- src/main/java/SequentialDigits.java
+++ src/main/java/SequentialDigits.java
@@ -1,10 +1,21 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class SequentialDigits {
     public List<Integer> sequentialDigits(int low, int high) {
-        return null;
+        List<Integer> resultList = new ArrayList<>();
+        for (int i=1; i <= 9; i++) {
+            int number = i;
+            int nextDigit = i + 1;
+            while (nextDigit <= 9 && number <= high) {
+                number = number*10 + nextDigit;
+                if (number >= low && number <= high) {
+                    resultList.add(number);
+                }
+                nextDigit++;
+            }
+        }
+        Collections.sort(resultList);
+        return resultList;
     }
 }
