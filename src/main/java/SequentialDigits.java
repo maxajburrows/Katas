758d224fb94c7cb041429355021347843490cb8d
Max Burrows <maxajburrows@gmail.com>
2024-02-02 12:08:26 +0100

    First solution to sequential digits kata

--- src/main/java/SequentialDigits.java
+++ src/main/java/SequentialDigits.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class SequentialDigits {
     public List<Integer> sequentialDigits(int low, int high) {
-
+        ArrayList<Integer> resultList = new ArrayList<>();
+        int[] testArray = IntStream.rangeClosed(low, high).toArray();
+        OUTER:
+        for (int integer : testArray) {
+            Integer[] splitInt = Arrays.stream(String.valueOf(integer)
+                            .split(""))
+                    .map(Integer::parseInt)
+                    .toArray(Integer[]::new);
+            for (int j = 0; j < splitInt.length - 1; j++) {
+                if (!splitInt[j].equals(splitInt[j + 1])) {
+                    continue OUTER;
+                }
+            }
+            resultList.add(integer);
+        }
+        return resultList;
     }
 }
