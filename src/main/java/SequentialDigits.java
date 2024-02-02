5c7bb68fcff909ea2fddf8590f9c6015e44446e2
Max Burrows <maxajburrows@gmail.com>
2024-02-02 13:07:03 +0100

    Try and improve runtime further

--- src/main/java/SequentialDigits.java
+++ src/main/java/SequentialDigits.java
@@ -20,9 +20,26 @@ public class SequentialDigits {
                 }
             }
             resultList.add(integer);
-            integer += (int) Math.pow(10, splitInt.size()-1);
+            while (!splitInt.get(splitInt.size()-1).equals(9) && integer <= high) {
+                for (int i=0; i < splitInt.size(); i++) {
+                    splitInt.set(i, splitInt.get(i)+1);
+                }
+                integer = convertToInt(splitInt);
+                if (integer <= high) {
+                    resultList.add(integer);
+                }
+            }
+            //integer += (int) Math.pow(10, splitInt.size()-1);
         }
         return resultList;
+    }
+
+    private int convertToInt(List<Integer> splitInt) {
+        int result = 0;
+        for (Integer digit : splitInt) {
+            result = result*10 + digit;
+        }
+        return result;
     }
 
     private void splitInteger(int integer, List<Integer> splitInt) {
