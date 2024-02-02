46e5d7cb986f1858091d791569bd85bac66f69fc
Max Burrows <maxajburrows@gmail.com>
2024-02-02 12:34:48 +0100

    Try to improve runtime further

--- src/main/java/SequentialDigits.java
+++ src/main/java/SequentialDigits.java
@@ -8,18 +8,26 @@ public class SequentialDigits {
         ArrayList<Integer> resultList = new ArrayList<>();
         OUTER:
         for (int integer=low; integer <= high; integer++) {
-            char[] splitChars = String.valueOf(integer).toCharArray();
-            int[] splitInt = new int[splitChars.length];
-            for (int i=0; i < splitChars.length; i++) {
-                splitInt[i] = Character.getNumericValue(splitChars[i]);
-            }
-            for (int j = 0; j < splitInt.length - 1; j++) {
-                if (splitInt[j] != (splitInt[j+1]-1)) {
+//            char[] splitChars = String.valueOf(integer).toCharArray();
+            List<Integer> splitInt = new ArrayList<>();
+            splitInteger(integer, splitInt);
+//            for (int i=0; i < splitChars.length; i++) {
+//                splitInt[i] = Character.getNumericValue(splitChars[i]);
+//            }
+            for (int j = 0; j < splitInt.size() - 1; j++) {
+                if (!splitInt.get(j).equals(splitInt.get(j+1)-1)) {
                     continue OUTER;
                 }
             }
             resultList.add(integer);
         }
         return resultList;
+    }
+
+    private void splitInteger(int integer, List<Integer> splitInt) {
+        if (integer / 10 > 0) {
+            splitInteger(integer/10, splitInt);
+        }
+        splitInt.add(integer % 10);
     }
 }
