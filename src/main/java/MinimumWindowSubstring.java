4f775089b6049266c012ccebcd1c34116f993686
Max Burrows <maxajburrows@gmail.com>
2024-02-04 17:29:51 +0100

    First run at full min window algorithm

--- src/main/java/MinimumWindowSubstring.java
+++ src/main/java/MinimumWindowSubstring.java
@@ -1,23 +1,32 @@
 public class MinimumWindowSubstring {
     public String minWindow(String s, String t) {
         // Loop through s once increasing the range as you go
-        String[] splitT = t.split("");
-        for (int i=t.length(); i <= s.length(); i++) {
-            String testString = s.substring(0, i);
-            String[] splitTestString = testString.split("");
-            int containedLetters = 0;
-            for (int j=0; j < splitT.length; j++) {
-                for (int k=0; k < splitTestString.length; k++) {
-                    if (splitT[j].equals(splitTestString[k])) {
-                        containedLetters++;
-                        break;
+        String resultString = "";
+        for (int l=0; l < s.length() - t.length(); l++) {
+            String[] splitT = t.split("");
+            for (int i=l+t.length(); i <= s.length(); i++) {
+                String testString = s.substring(l, i);
+                String[] splitTestString = testString.split("");
+                int containedLetters = 0;
+                for (int j=0; j < splitT.length; j++) {
+                    for (int k=0; k < splitTestString.length; k++) {
+                        if (splitT[j].equals(splitTestString[k])) {
+                            containedLetters++;
+                            break;
+                        }
                     }
                 }
+                if (l == 0 && containedLetters == splitT.length) {
+                    resultString = testString;
+                }
+                if (containedLetters == splitT.length && testString.length() < resultString.length()) {
+                    resultString = testString;
+                }
             }
-            if (containedLetters == splitT.length) {
-                return testString;
+            if (resultString.isEmpty()) {
+                return resultString;
             }
         }
-        return "";
+        return resultString;
     }
 }
