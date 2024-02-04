1517ca7961bbbc7c02f8ffb67aba862a15c7e7b0
Max Burrows <maxajburrows@gmail.com>
2024-02-04 16:53:56 +0100

    Write inner loop for algorithm

--- src/main/java/MinimumWindowSubstring.java
+++ src/main/java/MinimumWindowSubstring.java
@@ -1,6 +1,24 @@
 public class MinimumWindowSubstring {
 
     public String minWindow(String s, String t) {
+        // Loop through s once increasing the range as you go
+        String[] splitT = t.split("");
+        for (int i=t.length(); i <= s.length(); i++) {
+            String testString = s.substring(0, i);
+            String[] splitTestString = testString.split("");
+            int containedLetters = 0;
+            for (int j=0; j < splitT.length; j++) {
+                for (int k=0; k < splitTestString.length; k++) {
+                    if (splitT[i].equals(splitTestString[j])) {
+                        containedLetters++;
+                        break;
+                    }
+                }
+            }
+            if (containedLetters == splitT.length) {
+                return testString;
+            }
+        }
         return "";
     }
 }
