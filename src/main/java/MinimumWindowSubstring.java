16559d482c0d0027ce32cbd2da17a5d5ad66718e
Max Burrows <maxajburrows@gmail.com>
2024-02-04 18:10:29 +0100

    Slightly impove runtime of solution

--- src/main/java/MinimumWindowSubstring.java
+++ src/main/java/MinimumWindowSubstring.java
@@ -3,13 +3,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MinimumWindowSubstring {
+
+//    public String minWindow(String s, String t) {
+//        return "";
+//    }
     public String minWindow(String s, String t) {
         String resultString = "";
         for (int l=0; l <= s.length() - t.length(); l++) {
             String[] splitT = t.split("");
             for (int i=l+t.length(); i <= s.length(); i++) {
                 String testString = s.substring(l, i);
-                ArrayList<String> splitTestString = new ArrayList<String>(Arrays.asList(testString.split("")));
+                ArrayList<String> splitTestString = splitString(testString);
                 int containedLetters = 0;
                 for (int j=0; j < splitT.length; j++) {
                     for (int k=0; k < splitTestString.size(); k++) {
@@ -34,5 +38,13 @@ public class MinimumWindowSubstring {
             }
         }
         return resultString;
+    }
+
+    private ArrayList<String> splitString(String testString) {
+        ArrayList<String> resultList = new ArrayList<>();
+        for (char c : testString.toCharArray()) {
+            resultList.add(String.valueOf(c));
+        }
+        return resultList;
     }
 }
