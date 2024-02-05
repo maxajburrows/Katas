2a891841d88e59f48e918c47da6430e4bede42e3
Max Burrows <maxajburrows@gmail.com>
2024-02-05 13:28:00 +0100

    First attempt at algorithm

--- src/main/java/FirstUniqueCharacter.java
+++ src/main/java/FirstUniqueCharacter.java
@@ -1,5 +1,15 @@
 public class FirstUniqueCharacter {
     public int firstUniqChar(String s) {
-        return 0;
+        OUTER:
+        for (int i=0; i < s.length(); i++) {
+            char testChar = s.charAt(i);
+            for (int j=i+1; j < s.length(); j++) {
+                if (testChar == s.charAt(j)) {
+                    continue OUTER;
+                }
+            }
+            return i;
+        }
+        return -1;
     }
 }
