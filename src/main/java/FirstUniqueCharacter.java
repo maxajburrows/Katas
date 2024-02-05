a3212017002edc9d6174a18506c6d336be94aff5
Max Burrows <maxajburrows@gmail.com>
2024-02-05 14:28:45 +0100

    Try solution using hashmap

--- src/main/java/FirstUniqueCharacter.java
+++ src/main/java/FirstUniqueCharacter.java
@@ -1,5 +1,10 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class FirstUniqueCharacter {
-    public int firstUniqChar(String s) {
+    public int firstUniqCharNestedLoop(String s) {
+        // TODO: Solution is n2 time complexity - can it be improved.
         OUTER:
         for (int i=0; i < s.length(); i++) {
             char testChar = s.charAt(i);
@@ -9,6 +14,25 @@ public class FirstUniqueCharacter {
                 }
             }
             return i;
+        }
+        return -1;
+    }
+
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> characterMap = new HashMap<>();
+        Set<Integer> duplicatedSet = new HashSet<>();
+        for (int i=0; i < s.length(); i++) {
+            if (characterMap.containsKey(s.charAt(i))) {
+                duplicatedSet.add(characterMap.get(s.charAt(i)));
+                duplicatedSet.add(i);
+                continue;
+            }
+            characterMap.put(s.charAt(i), i);
+        }
+        for (int i=0; i < s.length(); i++) {
+            if (!duplicatedSet.contains(i)) {
+                return i;
+            }
         }
         return -1;
     }
