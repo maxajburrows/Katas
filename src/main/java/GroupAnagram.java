f56b75d54a9aba4ca98af70356acacdd747d09e3
Max Burrows <maxajburrows@gmail.com>
2024-02-07 13:50:59 +0100

    Start thinking about group anagram kata

--- src/main/java/GroupAnagram.java
+++ src/main/java/GroupAnagram.java
@@ -2,6 +2,10 @@ import java.util.List;
 
 public class GroupAnagram {
     public List<List<String>> groupAnagrams(String[] strs) {
+
+        // Use hashmap to store keys of each anagram?
+
+
         return null;
     }
 }
