4c256bfaa310e50aaf18f6b13dc2e74aa994cc74
Max Burrows <maxajburrows@gmail.com>
2024-02-22 14:37:09 +0100

    Write solution for solving kata

--- src/main/java/FindTheTownJudge.java
+++ src/main/java/FindTheTownJudge.java
@@ -1,5 +1,12 @@
 public class FindTheTownJudge {
     public int findJudge(int n, int[][] trust) {
 
+        // Look for index of trusted person
+
+        // Count the number of people that trust them - if not all -1
+
+        // See if trusted person trusts anyone - if they do return -1
+
+        return 0;
     }
 }
