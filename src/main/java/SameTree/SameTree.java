605b19bee7ff361f017ef0b435e0722208220fc0
Max Burrows <maxajburrows@gmail.com>
2024-02-26 12:31:21 +0100

    First attempt at same tree algorithm

--- src/main/java/SameTree/SameTree.java
+++ src/main/java/SameTree/SameTree.java
@@ -2,7 +2,21 @@ package SameTree;
 
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return false;
-
+        if (p.val != q.val) {
+            return false;
+        }
+        boolean leftTree;
+        boolean rightTree;
+        if (p.left == null && q.left == null) {
+            leftTree = true;
+        } else {
+            leftTree = isSameTree(p.left, q.left);
+        }
+        if (p.right == null && q.right == null) {
+            rightTree = true;
+        } else {
+            rightTree = isSameTree(p.right, q.right);
+        }
+        return leftTree && rightTree;
     }
 }
