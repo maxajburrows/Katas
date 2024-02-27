a9a372040e41788b548ed736782ea0bf235871ef
Max Burrows <maxajburrows@gmail.com>
2024-02-27 14:54:58 +0100

    First very basic attempt at diameter of binary tree algorithm

--- src/main/java/DiameterOfBinaryTree/DiameterOfBinaryTree.java
+++ src/main/java/DiameterOfBinaryTree/DiameterOfBinaryTree.java
@@ -2,6 +2,11 @@ package DiameterOfBinaryTree;
 
 public class DiameterOfBinaryTree {
     public int diameterOfBinaryTree(TreeNode root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+        int leftTreePathLength = diameterOfBinaryTree(root.left);
+        int rightTreePathLength = diameterOfBinaryTree(root.right);
+        return leftTreePathLength+rightTreePathLength+1;
     }
 }
