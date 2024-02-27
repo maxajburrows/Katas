56e560776f2362529979fc45667e83a257fd6f5f
Max Burrows <maxajburrows@gmail.com>
2024-02-27 17:32:27 +0100

    Get correct answer for majority of cases

--- src/main/java/DiameterOfBinaryTree/DiameterOfBinaryTree.java
+++ src/main/java/DiameterOfBinaryTree/DiameterOfBinaryTree.java
@@ -3,15 +3,15 @@ package DiameterOfBinaryTree;
 public class DiameterOfBinaryTree {
     public int diameterOfBinaryTree(TreeNode root) {
         if (root.left == null && root.right == null) {
-            return 1;
+            return 0;
         }
         int leftTreePathLength = 0;
         int rightTreePathLength = 0;
         if (root.left != null) {
-            leftTreePathLength = diameterOfBinaryTree(root.left);
+            leftTreePathLength = diameterOfBinaryTree(root.left) + 1;
         }
         if (root.right != null) {
-            rightTreePathLength = diameterOfBinaryTree(root.right);
+            rightTreePathLength = diameterOfBinaryTree(root.right) + 1;
         }
         return leftTreePathLength+rightTreePathLength;
     }
