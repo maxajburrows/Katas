a22446902d37586ceffeea3d9f90e0a108becd32
Max Burrows <maxajburrows@gmail.com>
2024-02-27 19:28:02 +0100

    Tidy code and pass all test cases

--- src/main/java/DiameterOfBinaryTree/DiameterOfBinaryTree.java
+++ src/main/java/DiameterOfBinaryTree/DiameterOfBinaryTree.java
@@ -12,25 +12,13 @@ public class DiameterOfBinaryTree {
         }
         int leftTreePathLength = 0;
         int rightTreePathLength = 0;
-        System.out.println("Made it here 1");
         if (root.left != null) {
             leftTreePathLength = diameterOfBinaryTreeHelper(root.left) + 1;
         }
         if (root.right != null) {
-            int rightFuncReturn = diameterOfBinaryTree(root.right);
-            System.out.println("rightFuncReturn" + rightFuncReturn);
             rightTreePathLength = diameterOfBinaryTreeHelper(root.right) + 1;
-            System.out.println("In here" + rightTreePathLength);
         }
-        System.out.println("Left length" + leftTreePathLength);
-        System.out.println("Right length" + leftTreePathLength);
-        System.out.println("Node value" + root.val);
         currentMax = Math.max(leftTreePathLength + rightTreePathLength, currentMax);
-        System.out.println("Current Max" + currentMax);
-        System.out.println(Math.max(leftTreePathLength, rightTreePathLength));
-        int returnValue = Math.max(leftTreePathLength, rightTreePathLength);
-        System.out.println(returnValue);
-        return returnValue;
+        return Math.max(leftTreePathLength, rightTreePathLength);
     }
-
 }
