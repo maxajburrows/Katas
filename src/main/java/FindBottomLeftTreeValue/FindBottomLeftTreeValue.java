45411fae2210e4f4ef5f2d440ceadd2a0ccfbf53
Max Burrows <maxajburrows@gmail.com>
2024-02-28 12:40:31 +0100

    Add comment on how to complete kata

--- src/main/java/FindBottomLeftTreeValue/FindBottomLeftTreeValue.java
+++ src/main/java/FindBottomLeftTreeValue/FindBottomLeftTreeValue.java
@@ -5,9 +5,13 @@ public class FindBottomLeftTreeValue {
         if (root.left == null && root.right == null) {
             return root.val;
         }
+        if (root.right == null) {
+            return findBottomLeftValue(root.left);
+        }
         if (root.left == null) {
             return findBottomLeftValue(root.right);
         }
+        // In case of neither is null how do you choose - need to count somehow: maybe make helper
         return findBottomLeftValue(root.left);
     }
 }
