2c42c884c4937303c9c15f5aa42a82496e939bbd
Max Burrows <maxajburrows@gmail.com>
2024-02-28 12:38:49 +0100

    First attempt at bottom left tree value kata

--- src/main/java/FindBottomLeftTreeValue/FindBottomLeftTreeValue.java
+++ src/main/java/FindBottomLeftTreeValue/FindBottomLeftTreeValue.java
@@ -2,6 +2,12 @@ package FindBottomLeftTreeValue;
 
 public class FindBottomLeftTreeValue {
     public int findBottomLeftValue(TreeNode root) {
-        return 0;
+        if (root.left == null && root.right == null) {
+            return root.val;
+        }
+        if (root.left == null) {
+            return findBottomLeftValue(root.right);
+        }
+        return findBottomLeftValue(root.left);
     }
 }
