9fb8880ac8bfe92c372730027ee9c2b4b18fc0ac
Max Burrows <maxajburrows@gmail.com>
2024-02-29 16:25:51 +0100

    First attempt at even odd tree kata

--- src/main/java/EvenOddTree/EvenOddTree.java
+++ src/main/java/EvenOddTree/EvenOddTree.java
@@ -1,9 +1,32 @@
 package EvenOddTree;
 
+import java.util.LinkedList;
+
 public class EvenOddTree {
     public boolean isEvenOddTree(TreeNode root) {
-        // Breadth first traversal
-        // Count the level (or a switch) - make sure conditions are met
-        return false;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.addLast(root);
+
+        int treeLevel = 0;
+        while (!queue.isEmpty()) {
+            int queueLength = queue.size();
+            for (int i=0; i < queueLength; i++) {
+                TreeNode newNode = queue.poll();
+                if (treeLevel%2 == 0 && newNode.val%2 == 0) {
+                    return false;
+                }
+                if (treeLevel%2 == 1 && newNode.val%2 == 1) {
+                    return false;
+                }
+                if (newNode.left != null) {
+                    queue.addLast(newNode.left);
+                }
+                if (newNode.right != null) {
+                    queue.addLast(newNode.right);
+                }
+                treeLevel++;
+            }
+        }
+        return true;
     }
 }
