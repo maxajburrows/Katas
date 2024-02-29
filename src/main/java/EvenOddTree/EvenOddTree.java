994ee930369a2dbee26c6262159dc93dc1682481
Max Burrows <maxajburrows@gmail.com>
2024-02-29 08:30:42 +0100

    Set up Even odd tree kata with first ideas on how to solve it

---
+++ src/main/java/EvenOddTree/EvenOddTree.java
@@ -0,0 +1,9 @@
+package EvenOddTree;
+
+public class EvenOddTree {
+    public boolean isEvenOddTree(TreeNode root) {
+        // Breadth first traversal
+        // Count the level (or a switch) - make sure conditions are met
+        return false;
+    }
+}
