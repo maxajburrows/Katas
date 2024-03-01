450e33fa0e286c6d5b4e207e9153d41cf3221c5e
Max Burrows <maxajburrows@gmail.com>
2024-03-01 13:49:38 +0100

    Tidy code

--- src/main/java/MaximumOddBinaryNumber.java
+++ src/main/java/MaximumOddBinaryNumber.java
@@ -1,14 +1,13 @@
 public class MaximumOddBinaryNumber {
     public String maximumOddBinaryNumber(String s) {
-        int length = s.length();
         int oneCounter = 0;
-        for (int i=0; i < length; i++) {
-            if (s.charAt(i) == '1') {
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
                 oneCounter++;
             }
         }
         String ones = (oneCounter > 0) ? "1".repeat(oneCounter-1): "";
-        String zeroes = "0".repeat(length - oneCounter);
+        String zeroes = "0".repeat(s.length() - oneCounter);
         return ones + zeroes + "1";
     }
 }
