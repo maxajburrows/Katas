827c677e0127ba8762376e7f619bdcf4eaa1b37d
Max Burrows <maxajburrows@gmail.com>
2024-03-01 13:36:58 +0100

    First attempt at maximum odd binary number algorithm

--- src/main/java/MaximumOddBinaryNumber.java
+++ src/main/java/MaximumOddBinaryNumber.java
@@ -1,5 +1,14 @@
 public class MaximumOddBinaryNumber {
     public String maximumOddBinaryNumber(String s) {
-        return null;
+        int length = s.length();
+        int oneCounter = 0;
+        for (int i=0; i < length; i++) {
+            if (s.charAt(0) == '1') {
+                oneCounter++;
+            }
+        }
+        String ones = (oneCounter > 0) ? "1".repeat(oneCounter-1): "";
+        String zeroes = (length - oneCounter > 0) ? "0".repeat(length - oneCounter - 1): "";
+        return ones + zeroes + "1";
     }
 }
