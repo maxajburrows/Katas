5e60ab2f2d58bcae719c241f60b4bdeb56bef18d
Max Burrows <maxajburrows@gmail.com>
2024-03-01 13:58:39 +0100

    Add faster solution

--- src/main/java/MaximumOddBinaryNumber.java
+++ src/main/java/MaximumOddBinaryNumber.java
@@ -6,6 +6,24 @@ public class MaximumOddBinaryNumber {
                 oneCounter++;
             }
         }
+        StringBuilder result = new StringBuilder();
+        for (int i=0; i < oneCounter-1; i++) {
+            result.append("1");
+        }
+        for (int i=0; i < s.length()-oneCounter; i++) {
+            result.append("0");
+        }
+        result.append("1");
+        return new String(result);
+    }
+
+    public String maximumOddBinaryNumberConcat(String s) {
+        int oneCounter = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                oneCounter++;
+            }
+        }
         String ones = (oneCounter > 0) ? "1".repeat(oneCounter-1): "";
         String zeroes = "0".repeat(s.length() - oneCounter);
         return ones + zeroes + "1";
