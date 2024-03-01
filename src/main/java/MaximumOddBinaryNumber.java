429b8d838b5a2b02e6a4a8655cb7322920cff620
Max Burrows <maxajburrows@gmail.com>
2024-03-01 13:46:26 +0100

    Fix bug and pass all test cases

--- src/main/java/MaximumOddBinaryNumber.java
+++ src/main/java/MaximumOddBinaryNumber.java
@@ -3,12 +3,12 @@ public class MaximumOddBinaryNumber {
         int length = s.length();
         int oneCounter = 0;
         for (int i=0; i < length; i++) {
-            if (s.charAt(0) == '1') {
+            if (s.charAt(i) == '1') {
                 oneCounter++;
             }
         }
         String ones = (oneCounter > 0) ? "1".repeat(oneCounter-1): "";
-        String zeroes = (length - oneCounter > 0) ? "0".repeat(length - oneCounter - 1): "";
+        String zeroes = "0".repeat(length - oneCounter);
         return ones + zeroes + "1";
     }
 }
