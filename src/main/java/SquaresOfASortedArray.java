1d9483725cd0ef6974dc7157c92c1a0d5e007232
Max Burrows <maxajburrows@gmail.com>
2024-03-02 19:22:07 +0100

    Pass all test cases with nlogn solution

--- src/main/java/SquaresOfASortedArray.java
+++ src/main/java/SquaresOfASortedArray.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class SquaresOfASortedArray {
     public int[] sortedSquares(int[] nums) {
-        return null;
+        int length = nums.length;
+        int[] squaresArray = new int[length];
+        for (int i=0; i < length; i++) {
+            squaresArray[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(squaresArray);
+        return squaresArray;
     }
 }
