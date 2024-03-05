4b8d8631e190e2b901baf4d7e57f4e5481060548
Max Burrows <maxajburrows@gmail.com>
2024-03-05 16:00:19 +0100

    First attempt at algorithm

--- src/main/java/RemoveNthNodeFromEndOfList/RemoveNthNodeFromEndOfList.java
+++ src/main/java/RemoveNthNodeFromEndOfList/RemoveNthNodeFromEndOfList.java
@@ -2,10 +2,17 @@ package RemoveNthNodeFromEndOfList;
 
 public class RemoveNthNodeFromEndOfList {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode traverseNode = head;
+        int listLength = 1;
+        while (traverseNode.next != null) {
+            traverseNode = traverseNode.next;
+            listLength++;
+        }
         ListNode currentNode = head;
-        for (int i=0; i < n; i++) {
+        for (int i=0; i < listLength-n; i++) {
             currentNode = currentNode.next;
         }
-        return null;
+        currentNode.next = currentNode.next.next;
+        return head;
     }
 }
