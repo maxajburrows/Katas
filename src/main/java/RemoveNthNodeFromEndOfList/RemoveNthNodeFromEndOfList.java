6f0503f5e0334ccdab4f2c930e7154a174f61da5
Max Burrows <maxajburrows@gmail.com>
2024-03-05 15:52:51 +0100

    Start trying to solve kata

--- src/main/java/RemoveNthNodeFromEndOfList/RemoveNthNodeFromEndOfList.java
+++ src/main/java/RemoveNthNodeFromEndOfList/RemoveNthNodeFromEndOfList.java
@@ -2,6 +2,10 @@ package RemoveNthNodeFromEndOfList;
 
 public class RemoveNthNodeFromEndOfList {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode currentNode = head;
+        for (int i=0; i < n; i++) {
+            currentNode = currentNode.next;
+        }
         return null;
     }
 }
