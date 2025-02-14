d19382eeeb12849d93ed56bf439a1a42c5e0fefa
Max Burrows <maxajburrows@gmail.com>
2025-02-14 21:05:56 +0100

    Simplify two pass hashmap solution

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -5,22 +5,15 @@ import java.util.HashMap;
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> indexMap = new HashMap<>();
-        ArrayList<Integer> duplicates = new ArrayList<>();
         for (int i=0; i < nums.length; i++) {
-            if (indexMap.containsKey(nums[i])) {
-                duplicates.add(nums[i]);
-            }
             indexMap.put(nums[i], i);
         }
 
         for (int i=0; i < nums.length; i++) {
             int subtract = target - nums[i];
-            if (indexMap.containsKey(subtract)) {
-                if (indexMap.get(subtract) == i && !duplicates.contains(subtract)) {
-                    continue;
+            if (indexMap.containsKey(subtract) && indexMap.get(subtract) != i) {
+                    return new int[] {i, indexMap.get(subtract)};
                 }
-                return new int[] {i, indexMap.get(subtract)};
-            }
         }
         return new int[0];
     }
