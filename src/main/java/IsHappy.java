21d46aec3bb055aec63a0140eccf35bc01a29daa
Max Burrows <maxajburrows@gmail.com>
2025-02-14 22:38:58 +0100

    Simplify happy number solution

--- src/main/java/IsHappy.java
+++ src/main/java/IsHappy.java
@@ -1,26 +1,21 @@
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class IsHappy {
     HashSet<Integer> seenNumbers = new HashSet<>();
-    int count = 0;
     public boolean isHappy(int n) {
-        return splitN(n);
-    }
-    private boolean splitN(int n) {
-        int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
+        while (n != 1) {
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            if (seenNumbers.contains(sum)) {
+                return false;
+            }
+            seenNumbers.add(sum);
+            n = sum;
         }
-        if (sum == 1) {
-            return true;
-        }
-        if (seenNumbers.contains(sum)) {
-            return false;
-        }
-        seenNumbers.add(sum);
-        return splitN(sum);
+        return true;
     }
 }
