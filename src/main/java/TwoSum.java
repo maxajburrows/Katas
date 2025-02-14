d2acee2828b62112fbd3ffe9666960919f070d02
Max Burrows <maxajburrows@gmail.com>
2025-02-14 20:59:57 +0100

    Proper hashmap solution to two sums problem

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -1,31 +1,25 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> indexMap = new HashMap<>();
-        HashMap<Integer, Integer> duplicateMap = new HashMap<>();
+        ArrayList<Integer> duplicates = new ArrayList<>();
         for (int i=0; i < nums.length; i++) {
             if (indexMap.containsKey(nums[i])) {
-                duplicateMap.put(nums[i], i);
-                continue;
+                duplicates.add(nums[i]);
             }
             indexMap.put(nums[i], i);
         }
-        Arrays.sort(nums);
 
         for (int i=0; i < nums.length; i++) {
-            for (int j=i+1; j < nums.length; j++) {
-                int sum = nums[i] + nums[j];
-                if (sum == target) {
-                    if (nums[i] == nums[j]) {
-                        return new int[] {indexMap.get(nums[i]), duplicateMap.get(nums[i])};
-                    }
-                    return new int[] {indexMap.get(nums[i]), indexMap.get(nums[j])};
+            int subtract = target - nums[i];
+            if (indexMap.containsKey(subtract)) {
+                if (indexMap.get(subtract) == i && !duplicates.contains(subtract)) {
+                    continue;
                 }
-                if (sum > target) {
-                    break;
-                }
+                return new int[] {i, indexMap.get(subtract)};
             }
         }
         return new int[0];
