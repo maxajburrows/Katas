69453f53999a0b8384768a180464a45fd4f0bf96
Max Burrows <maxajburrows@gmail.com>
2025-02-14 21:12:45 +0100

    Implement one pass hashmap solution to two sum problem

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -1,19 +1,14 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> indexMap = new HashMap<>();
         for (int i=0; i < nums.length; i++) {
-            indexMap.put(nums[i], i);
-        }
-
-        for (int i=0; i < nums.length; i++) {
             int subtract = target - nums[i];
-            if (indexMap.containsKey(subtract) && indexMap.get(subtract) != i) {
-                    return new int[] {i, indexMap.get(subtract)};
-                }
+            if (indexMap.containsKey(subtract)) {
+                return new int[] {indexMap.get(subtract), i};
+            }
+            indexMap.put(nums[i], i);
         }
         return new int[0];
     }
