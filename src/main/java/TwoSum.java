381cd92083cb99a49a63472be5f73f554bcb6103
Max Burrows <maxajburrows@gmail.com>
2025-02-14 20:29:37 +0100

    Two sum sorting and using hashmap

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -1,12 +1,30 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[2];
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
+        HashMap<Integer, Integer> duplicateMap = new HashMap<>();
+        for (int i=0; i < nums.length; i++) {
+            if (indexMap.containsKey(nums[i])) {
+                duplicateMap.put(nums[i], i);
+                continue;
+            }
+            indexMap.put(nums[i], i);
+        }
+        Arrays.sort(nums);
+
         for (int i=0; i < nums.length; i++) {
             for (int j=i+1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    answer[0] = i;
-                    answer[1] = j;
-                    return answer;
+                int sum = nums[i] + nums[j];
+                if (sum == target) {
+                    if (nums[i] == nums[j]) {
+                        return new int[] {indexMap.get(nums[i]), duplicateMap.get(nums[i])};
+                    }
+                    return new int[] {indexMap.get(nums[i]), indexMap.get(nums[j])};
+                }
+                if (sum > target) {
+                    break;
                 }
             }
         }
