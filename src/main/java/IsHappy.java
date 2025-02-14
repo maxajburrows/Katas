dbae3f9b0d5b8a3d4d7d1ad2d213730fc78c12e8
Max Burrows <maxajburrows@gmail.com>
2025-02-14 22:19:33 +0100

    Correct solution to isHappy kata

--- src/main/java/IsHappy.java
+++ src/main/java/IsHappy.java
@@ -2,8 +2,25 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class IsHappy {
+    HashSet<Integer> seenNumbers = new HashSet<>();
+    int count = 0;
     public boolean isHappy(int n) {
-        HashSet<Integer> seenNumbers = new HashSet<>();
-        return false;
+        return splitN(n);
+    }
+    private boolean splitN(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        if (sum == 1) {
+            return true;
+        }
+        if (seenNumbers.contains(sum)) {
+            return false;
+        }
+        seenNumbers.add(sum);
+        return splitN(sum);
     }
 }
