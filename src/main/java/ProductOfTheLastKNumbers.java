bddefbdfc319881c918c3d90f63dbb07c0691965
Max Burrows <maxajburrows@gmail.com>
2025-02-14 17:28:52 +0100

    Set up ProductofTheLastKNumbers kata

---
+++ src/main/java/ProductOfTheLastKNumbers.java
@@ -0,0 +1,13 @@
+public class ProductOfTheLastKNumbers {
+    public void ProductOfNumbers() {
+
+    }
+
+    public void add(int num) {
+
+    }
+
+    public int getProduct(int k) {
+        return 0;
+    }
+}
