7502c22bcf09b3cd3ae88cad9e6237d6799bc0bc
Max Burrows <maxajburrows@gmail.com>
2025-02-15 12:57:59 +0100

    Complete intersection of array problem

--- src/main/java/IntersectionOfTwoArrays.java
+++ src/main/java/IntersectionOfTwoArrays.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class IntersectionOfTwoArrays {
     public int[] intersection(int[] nums1, int[] nums2) {
-        return new int[0];
+        Set<Integer> resultSet = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                resultSet.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        int[] resultArray = new int[resultSet.size()];
+        Integer[] result = resultSet.toArray(new Integer[0]);
+        for (int k = 0; k < resultArray.length; k++) {
+            resultArray[k] = result[k];
+        }
+        return resultArray;
     }
 }
