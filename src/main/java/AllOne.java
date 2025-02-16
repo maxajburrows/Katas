9d98d6fa46f1bcfef27f6b08c60f79ecac3cee89
Max Burrows <maxajburrows@gmail.com>
2025-02-16 18:13:53 +0100

    Set up AllOne kata

---
+++ src/main/java/AllOne.java
@@ -0,0 +1,21 @@
+public class AllOne {
+    public AllOne() {
+
+    }
+
+    public void inc(String key) {
+
+    }
+
+    public void dec(String key) {
+
+    }
+
+    public String getMaxKey() {
+        return null;
+    }
+
+    public String getMinKey() {
+        return null;
+    }
+}
