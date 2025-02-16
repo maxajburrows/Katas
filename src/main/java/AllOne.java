b798cbaf6b8fbe42a89053f619131537fe5d5b04
Max Burrows <maxajburrows@gmail.com>
2025-02-16 18:41:00 +0100

    First attempt of AllOne problem

--- src/main/java/AllOne.java
+++ src/main/java/AllOne.java
@@ -1,21 +1,51 @@
-public class AllOne {
-    public AllOne() {
+import java.util.HashMap;
 
+public class AllOne {
+    HashMap<String, Integer> keyMap;
+    int max;
+    int min;
+    String maxKey;
+    String minKey;
+    public AllOne() {
+        keyMap = new HashMap<>();
+        maxKey = "";
+        minKey = "";
+        max = 1;
+        min = 1;
     }
 
     public void inc(String key) {
-
+        if (keyMap.containsKey(key)) {
+            int value = keyMap.get(key);
+            if (value == max) {
+                max++;
+                maxKey = key;
+            }
+            keyMap.put(key, keyMap.get(key) + 1);
+            return;
+        }
+        keyMap.put(key, 1);
+        minKey = key;
     }
 
     public void dec(String key) {
-
+        if (keyMap.get(key) == 1) {
+            keyMap.remove(key);
+            return;
+        }
+        int value = keyMap.get(key);
+        if (value == min) {
+            min--;
+            minKey = key;
+        }
+        keyMap.put(key, keyMap.get(key) - 1);
     }
 
     public String getMaxKey() {
-        return null;
+        return maxKey;
     }
 
     public String getMinKey() {
-        return null;
+        return minKey;
     }
 }
