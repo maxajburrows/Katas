90f51a6eee1fcc5b11b0e7c4f81ab003fa924888
Max Burrows <maxajburrows@gmail.com>
2025-02-17 18:52:36 +0100

    Count number of duplicates for NumTilePossibilities

--- src/main/java/NumTilePossibilities.java
+++ src/main/java/NumTilePossibilities.java
@@ -1,5 +1,21 @@
+import java.util.HashSet;
+
 public class NumTilePossibilities {
     public int numTilePossibilities(String tiles) {
+        int numDuplicates = findNumDuplicates(tiles);
         return 0;
+    }
+
+    private static int findNumDuplicates(String tiles) {
+        HashSet<Character> orginalLetters = new HashSet<>();
+        int numDuplicates = 0;
+        for (char letter : tiles.toCharArray()) {
+            if (orginalLetters.contains(letter)) {
+                numDuplicates++;
+                continue;
+            }
+            orginalLetters.add(letter);
+        }
+        return numDuplicates;
     }
 }
