fa1e1d7cbcbd5c1aa1c7211ff46391c021797016
Max Burrows <maxajburrows@gmail.com>
2025-02-18 20:20:29 +0100

    Start Permutations kata

--- src/main/java/Permutations.java
+++ src/main/java/Permutations.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations {
     public List<List<Integer>> permute(int[] nums) {
-        return null;
+        List<List<Integer>> permutations = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int[] newNums = new int[nums.length - 1];
+
+
+        }
+        return permutations;
     }
 }
