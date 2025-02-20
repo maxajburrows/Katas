a678e77557f7c82cb50b4d4058c6ef7c37ccd156
Max Burrows <maxajburrows@gmail.com>
2025-02-21 00:35:29 +0100

    Simplify add binary solution again

--- src/main/java/AddBinary.java
+++ src/main/java/AddBinary.java
@@ -1,24 +1,18 @@
 public class AddBinary {
     public String addBinary(String a, String b) {
-        String longer = a.length() > b.length() ? a : b;
-        String shorter = a.length() > b.length() ? b : a;
         StringBuilder result = new StringBuilder();
         int carry = 0;
-        for (int i = 0; i < shorter.length(); i++) {
-            int shortEndChar = shorter.charAt(shorter.length() - 1 - i) - '0';
-            int longEndChar = longer.charAt(longer.length() - 1 - i) - '0';
-            int sum = shortEndChar + longEndChar + carry;
-            carry = sum / 2;
-            result.append(sum % 2);
-        }
-        for (int i = shorter.length(); i < longer.length(); i++) {
-            int longEndChar = longer.charAt(longer.length() - 1 - i) - '0';
-            int sum = longEndChar + carry;
-            carry = sum / 2;
-            result.append(sum % 2);
-        }
-        if (carry == 1) {
-            result.append("1");
+        int pointer1 = a.length() - 1;
+        int pointer2 = b.length() - 1;
+        while (pointer1 >= 0 || pointer2 >= 0 || carry > 0) {
+            if (pointer1 >= 0) {
+                carry += a.charAt(pointer1--) - '0';
+            }
+            if (pointer2 >= 0) {
+                carry += b.charAt(pointer2--) - '0';
+            }
+            result.append(carry % 2);
+            carry /= 2;
         }
         return result.reverse().toString();
     }
