b506213ae0bbb68f15db5bc9c89beda799351dbc
Max Burrows <maxajburrows@gmail.com>
2025-02-22 21:42:10 +0100

    Solve minimum absolute difference

--- src/main/java/leetcode/inProgress/minimumAbsoluteDifferenceInBst/MinimumAbsoluteDifferenceInBst530.java
+++ src/main/java/leetcode/inProgress/minimumAbsoluteDifferenceInBst/MinimumAbsoluteDifferenceInBst530.java
@@ -2,17 +2,22 @@ package leetcode.inProgress.minimumAbsoluteDifferenceInBst;
 
 public class MinimumAbsoluteDifferenceInBst530 {
     int difference = Integer.MAX_VALUE;
+    int lastValue = -100_001;
     public int getMinimumDifference(TreeNode root) {
-        if (root.left != null) {
-            int leftDifference = Math.abs(root.val - root.left.val);
-            difference = Math.min(difference, leftDifference);
-            getMinimumDifference(root.left);
-        }
-        if (root.right != null) {
-            int rightDifference = Math.abs(root.val - root.right.val);
-            difference = Math.min(difference, rightDifference);
-            getMinimumDifference(root.right);
-        }
+        inOrderTraversal(root);
         return difference;
+    }
+
+    public void inOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrderTraversal(root.left);
+
+        int nextValue = root.val;
+        difference = Math.min(nextValue - lastValue, difference);
+        lastValue = nextValue;
+
+        inOrderTraversal(root.right);
     }
 }
