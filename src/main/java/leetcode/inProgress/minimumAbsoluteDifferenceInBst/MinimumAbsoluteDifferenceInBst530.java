aa515f018c1f5953f3b3ba92b2171fe89a4cbabc
Max Burrows <maxajburrows@gmail.com>
2025-02-22 16:35:15 +0100

    Set up minimum difference

---
+++ src/main/java/leetcode/inProgress/minimumAbsoluteDifferenceInBst/MinimumAbsoluteDifferenceInBst530.java
@@ -0,0 +1,7 @@
+package leetcode.inProgress.minimumAbsoluteDifferenceInBst;
+
+public class MinimumAbsoluteDifferenceInBst530 {
+    public int getMinimumDifference(TreeNode root) {
+        return 0;
+    }
+}
