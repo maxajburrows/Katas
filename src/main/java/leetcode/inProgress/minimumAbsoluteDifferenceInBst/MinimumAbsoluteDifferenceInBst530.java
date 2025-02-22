3f8f05523eecfe337b9081f6e094f8cdb0cd9ff8
Max Burrows <maxajburrows@gmail.com>
2025-02-22 17:01:51 +0100

    Complete getMinimumDifference

--- src/main/java/leetcode/inProgress/minimumAbsoluteDifferenceInBst/MinimumAbsoluteDifferenceInBst530.java
+++ src/main/java/leetcode/inProgress/minimumAbsoluteDifferenceInBst/MinimumAbsoluteDifferenceInBst530.java
@@ -1,7 +1,18 @@
 package leetcode.inProgress.minimumAbsoluteDifferenceInBst;
 
 public class MinimumAbsoluteDifferenceInBst530 {
+    int difference = Integer.MAX_VALUE;
     public int getMinimumDifference(TreeNode root) {
-        return 0;
+        if (root.left != null) {
+            int leftDifference = Math.abs(root.val - root.left.val);
+            difference = Math.min(difference, leftDifference);
+            getMinimumDifference(root.left);
+        }
+        if (root.right != null) {
+            int rightDifference = Math.abs(root.val - root.right.val);
+            difference = Math.min(difference, rightDifference);
+            getMinimumDifference(root.right);
+        }
+        return difference;
     }
 }
