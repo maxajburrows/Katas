d323d25c5e73e80e30b8db2ba8cfe21ca7c27604
Max Burrows <maxajburrows@gmail.com>
2025-02-22 16:31:34 +0100

    Start convert array to bst

--- src/main/java/leetcode/inProgress/convertSortedArrayToBinarySearchTree/convertSortedArrayToBinarySearchTree.java
+++ src/main/java/leetcode/inProgress/convertSortedArrayToBinarySearchTree/convertSortedArrayToBinarySearchTree.java
@@ -2,6 +2,14 @@ package leetcode.inProgress.convertSortedArrayToBinarySearchTree;
 
 public class convertSortedArrayToBinarySearchTree {
     public TreeNode sortedArrayToBST(int[] nums) {
+        int position = nums.length/2;
+        TreeNode head = new TreeNode(nums[position]);
+        head.left = buildTree(nums, position - 1);
+        head.right = buildTree(nums, position + 1);
+        return head;
+    }
+
+    private TreeNode buildTree(int[] nums, int position) {
         return null;
     }
 }
