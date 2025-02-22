40746d9a3901da93686cd7c5eb972c2dde35fd18
Max Burrows <maxajburrows@gmail.com>
2025-02-22 18:27:26 +0100

    First attempt averageOfLevels

--- src/main/java/leetcode/inProgress/AverageOfLevels.java
+++ src/main/java/leetcode/inProgress/AverageOfLevels.java
@@ -1,23 +1,38 @@
 package leetcode.inProgress;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
+import java.util.Queue;
 
 import leetcode.TreeNode;
 
 public class AverageOfLevels {
     List<Double> levelAverages = new ArrayList<>();
-    Stack<Double> valueStack = new Stack<>();
+    Queue<TreeNode> nodeQueue = new LinkedList<>();
     public List<Double> averageOfLevels(TreeNode root) {
-        levelAverages.add((double) root.val);
-
-        return levelAverages;
-    }
-
-    public void averageOfLevelsHelper(TreeNode root) {
-        if (root == null) {
-            return;
+        nodeQueue.add(root);
+        int level = 0;
+        int levelNodesLeft = 1;
+        int total = 0;
+        int divisor = 0;
+        while (!nodeQueue.isEmpty()) {
+            TreeNode nextNode = nodeQueue.poll();
+            levelNodesLeft--;
+            if (nextNode == null) {
+                continue;
+            }
+            total += nextNode.val;
+            divisor++;
+            if (levelNodesLeft == 0) {
+                Double levelAverage = ((double) total) / divisor;
+                levelAverages.add(levelAverage);
+                level++;
+                levelNodesLeft = (int) Math.pow(level, 2);
+            }
+            nodeQueue.add(nextNode.left);
+            nodeQueue.add(nextNode.right);
         }
+        return levelAverages;
     }
 }
