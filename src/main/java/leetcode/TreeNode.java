d322f54ab90430dc72c823d92b43b0bc39887d59
Max Burrows <maxajburrows@gmail.com>
2025-02-22 17:09:33 +0100

    Set up average of levels

---
+++ src/main/java/leetcode/TreeNode.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int x) {
+        val = x;
+    }
+    TreeNode(int x, TreeNode left, TreeNode right) {
+        val = x;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ src/main/java/leetcode/inProgress/AverageOfLevels.java
@@ -0,0 +1,14 @@
+package leetcode.inProgress;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import leetcode.TreeNode;
+
+public class AverageOfLevels {
+    List<Double> levelAverages = new ArrayList<>();
+    public List<Double> averageOfLevels(TreeNode root) {
+        return levelAverages;
+
+    }
+}
