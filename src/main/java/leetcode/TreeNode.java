d2f1030825a0ce52b7f4d25029a98ea216328b55
Max Burrows <maxajburrows@gmail.com>
2025-02-22 17:28:33 +0100

    Attempt average of levels

--- src/main/java/leetcode/TreeNode.java
+++ src/main/java/leetcode/TreeNode.java
@@ -1,14 +1,14 @@
 package leetcode;
 
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int x) {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int x) {
         val = x;
     }
-    TreeNode(int x, TreeNode left, TreeNode right) {
+    public TreeNode(int x, TreeNode left, TreeNode right) {
         val = x;
         this.left = left;
         this.right = right;
--- src/main/java/leetcode/inProgress/AverageOfLevels.java
+++ src/main/java/leetcode/inProgress/AverageOfLevels.java
@@ -2,13 +2,22 @@ package leetcode.inProgress;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import leetcode.TreeNode;
 
 public class AverageOfLevels {
     List<Double> levelAverages = new ArrayList<>();
+    Stack<Double> valueStack = new Stack<>();
     public List<Double> averageOfLevels(TreeNode root) {
-        return levelAverages;
+        levelAverages.add((double) root.val);
 
+        return levelAverages;
+    }
+
+    public void averageOfLevelsHelper(TreeNode root) {
+        if (root == null) {
+            return;
+        }
     }
 }
